eb0bd1b7e8d89f21bf708deaa256b8380755886c
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-11-01 11:50:36 -0400

    portfolio files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+// import About from "./components/About";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About';
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <About />
+        <Footer />
+      </div>
+    </Router>
   );
-}
-
-export default App;
+};
