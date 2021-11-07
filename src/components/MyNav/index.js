237d6957bf2413b4b2f1cb71d0c1c217464d582f
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-11-07 15:27:35 -0500

    change href

--- src/components/MyNav/index.js
+++ src/components/MyNav/index.js
@@ -14,14 +14,14 @@ function MyNav() {
     
       <Nav className="justify-content-end" bg="dark" variant="dark" expand="lg" sticky="top">
         <Nav.Item>
-          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="about">About</Nav.Link>
         </Nav.Item>
-          <Nav.Link href="/contactForm">Contact</Nav.Link>
+          <Nav.Link href="contactForm">Contact</Nav.Link>
         <Nav.Item>
-          <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+          <Nav.Link href="portfolio">Portfolio</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/resume">Resume</Nav.Link>
+          <Nav.Link href="resume">Resume</Nav.Link>
         </Nav.Item>
       </Nav>
 
